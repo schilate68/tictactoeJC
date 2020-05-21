3f8b61ff9004bb61c85c3d02087f63221d88a21d
Juan <lrccfj@outlook.com>
2020-05-21 10:36:45 +0100

    Final logic winning

--- ticTac.js
+++ ticTac.js
@@ -2,47 +2,67 @@ document.addEventListener('DOMContentLoaded', function () {
 
     let playerTurnState = false;
 
+
     for (let i = 1; i <= 9; i++) {
         document.getElementById(i.toString()).addEventListener("click", addLetter);
 
-        function addLetter() {
+        function checkWinner() {
 
+            if (document.getElementById("1").innerHTML === '<span class="x">X</span>' && document.getElementById("2").innerHTML === '<span class="x">X</span>' && document.getElementById("3").innerHTML === '<span class="x">X</span>') {
+                document.getElementById('result-message').innerHTML = "You won!";
+            } else if (document.getElementById("1").innerHTML === '<span class="o">O</span>' && document.getElementById("2").innerHTML === '<span class="o">O</span>' && document.getElementById("3").innerHTML === '<span class="o">O</span>') {
+                document.getElementById('result-message').innerHTML = "You won!";
+            } else if (document.getElementById("4").innerHTML === '<span class="x">X</span>' && document.getElementById("5").innerHTML === '<span class="x">X</span>' && document.getElementById("6").innerHTML === '<span class="x">X</span>') {
+                document.getElementById('result-message').innerHTML = "You won!";
+            } else if (document.getElementById("4").innerHTML === '<span class="o">O</span>' && document.getElementById("5").innerHTML === '<span class="o">O</span>' && document.getElementById("6").innerHTML === '<span class="o">O</span>') {
+                document.getElementById('result-message').innerHTML = "You won!";
+            } else if (document.getElementById("7").innerHTML === '<span class="o">O</span>' && document.getElementById("8").innerHTML === '<span class="o">O</span>' && document.getElementById("9").innerHTML === '<span class="o">O</span>') {
+                document.getElementById('result-message').innerHTML = "You won!";
+            } else if (document.getElementById("7").innerHTML === '<span class="x">X</span>' && document.getElementById("8").innerHTML === '<span class="x">X</span>' && document.getElementById("9").innerHTML === '<span class="x">X</span>') {
+                document.getElementById('result-message').innerHTML = "You won!";
+            } else if (document.getElementById("1").innerHTML === '<span class="x">X</span>' && document.getElementById("4").innerHTML === '<span class="x">X</span>' && document.getElementById("7").innerHTML === '<span class="x">X</span>') {
+                document.getElementById('result-message').innerHTML = "You won!";
+            } else if (document.getElementById("1").innerHTML === '<span class="o">O</span>' && document.getElementById("4").innerHTML === '<span class="o">O</span>' && document.getElementById("7").innerHTML === '<span class="o">O</span>') {
+                document.getElementById('result-message').innerHTML = "You won!";
+            } else if (document.getElementById("2").innerHTML === '<span class="x">X</span>' && document.getElementById("5").innerHTML === '<span class="x">X</span>' && document.getElementById("8").innerHTML === '<span class="x">X</span>') {
+                document.getElementById('result-message').innerHTML = "You won!";
+            } else if (document.getElementById("2").innerHTML === '<span class="o">O</span>' && document.getElementById("5").innerHTML === '<span class="o">O</span>' && document.getElementById("8").innerHTML === '<span class="o">O</span>') {
+                document.getElementById('result-message').innerHTML = "You won!";
+            } else if (document.getElementById("3").innerHTML === '<span class="x">X</span>' && document.getElementById("6").innerHTML === '<span class="x">X</span>' && document.getElementById("9").innerHTML === '<span class="x">X</span>') {
+                document.getElementById('result-message').innerHTML = "You won!";
+            } else if (document.getElementById("3").innerHTML === '<span class="o">O</span>' && document.getElementById("6").innerHTML === '<span class="o">O</span>' && document.getElementById("9").innerHTML === '<span class="o">O</span>') {
+                document.getElementById('result-message').innerHTML = "You won!";
+            } else if (document.getElementById("1").innerHTML === '<span class="x">X</span>' && document.getElementById("5").innerHTML === '<span class="x">X</span>' && document.getElementById("9").innerHTML === '<span class="x">X</span>') {
+                document.getElementById('result-message').innerHTML = "You won!";
+            } else if (document.getElementById("1").innerHTML === '<span class="o">O</span>' && document.getElementById("5").innerHTML === '<span class="o">O</span>' && document.getElementById("9").innerHTML === '<span class="o">O</span>') {
+                document.getElementById('result-message').innerHTML = "You won!";
+            } else if (document.getElementById("3").innerHTML === '<span class="x">X</span>' && document.getElementById("5").innerHTML === '<span class="x">X</span>' && document.getElementById("7").innerHTML === '<span class="x">X</span>') {
+                document.getElementById('result-message').innerHTML = "You won!";
+            } else if (document.getElementById("3").innerHTML === '<span class="o">O</span>' && document.getElementById("5").innerHTML === '<span class="o">O</span>' && document.getElementById("7").innerHTML === '<span class="o">O</span>') {
+                document.getElementById('result-message').innerHTML = "You won!";
+            }
+        }
+
+        function addLetter() {
 
             if (playerTurnState) {
                 if (!document.getElementById(i.toString()).innerHTML) {
                     document.getElementById(i.toString()).insertAdjacentHTML("afterbegin", '<span class="x">X</span>');
                     playerTurnState = false;
+                    checkWinner();
                 }
 
             } else if ((!document.getElementById(i.toString()).innerHTML)) {
                 document.getElementById(i.toString()).insertAdjacentHTML("afterbegin", '<span class="o">O</span>');
                 playerTurnState = true;
-
-            }
-
-
-            if (document.getElementById(i.toString()).innerHTML) {
-                if (document.getElementById("1").innerHTML === document.getElementById("2").innerHTML && document.getElementById("2").innerHTML === document.getElementById("3").innerHTML) {
-
-                    document.getElementById('result-message').innerHTML = "You won!";
+                checkWinner();
 
 
-                }
             }
 
 
         }
-
-
-
-
-
     }
-
-
-
-
-
 });
 
 
