1d927fcbbfcba43b92a87ad2432f8fcf0332b7f9
Juan <lrccfj@outlook.com>
2020-05-22 15:19:44 +0100

    Added restart button and functionality

--- ticTac.js
+++ ticTac.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     let playerTurnState = false;
+    let winnerState = false;
 
 
     for (let i = 1; i <= 9; i++) {
@@ -10,38 +11,82 @@ document.addEventListener('DOMContentLoaded', function () {
 
             if (document.getElementById("1").innerHTML === '<span class="x">X</span>' && document.getElementById("2").innerHTML === '<span class="x">X</span>' && document.getElementById("3").innerHTML === '<span class="x">X</span>') {
                 document.getElementById('result-message').innerHTML = "You won!";
+                winnerState = true;
             } else if (document.getElementById("1").innerHTML === '<span class="o">O</span>' && document.getElementById("2").innerHTML === '<span class="o">O</span>' && document.getElementById("3").innerHTML === '<span class="o">O</span>') {
                 document.getElementById('result-message').innerHTML = "You won!";
+                winnerState = true;
             } else if (document.getElementById("4").innerHTML === '<span class="x">X</span>' && document.getElementById("5").innerHTML === '<span class="x">X</span>' && document.getElementById("6").innerHTML === '<span class="x">X</span>') {
                 document.getElementById('result-message').innerHTML = "You won!";
+                winnerState = true;
             } else if (document.getElementById("4").innerHTML === '<span class="o">O</span>' && document.getElementById("5").innerHTML === '<span class="o">O</span>' && document.getElementById("6").innerHTML === '<span class="o">O</span>') {
                 document.getElementById('result-message').innerHTML = "You won!";
+                winnerState = true;
             } else if (document.getElementById("7").innerHTML === '<span class="o">O</span>' && document.getElementById("8").innerHTML === '<span class="o">O</span>' && document.getElementById("9").innerHTML === '<span class="o">O</span>') {
                 document.getElementById('result-message').innerHTML = "You won!";
+                winnerState = true;
             } else if (document.getElementById("7").innerHTML === '<span class="x">X</span>' && document.getElementById("8").innerHTML === '<span class="x">X</span>' && document.getElementById("9").innerHTML === '<span class="x">X</span>') {
                 document.getElementById('result-message').innerHTML = "You won!";
+                winnerState = true;
             } else if (document.getElementById("1").innerHTML === '<span class="x">X</span>' && document.getElementById("4").innerHTML === '<span class="x">X</span>' && document.getElementById("7").innerHTML === '<span class="x">X</span>') {
                 document.getElementById('result-message').innerHTML = "You won!";
+                winnerState = true;
             } else if (document.getElementById("1").innerHTML === '<span class="o">O</span>' && document.getElementById("4").innerHTML === '<span class="o">O</span>' && document.getElementById("7").innerHTML === '<span class="o">O</span>') {
                 document.getElementById('result-message').innerHTML = "You won!";
+                winnerState = true;
             } else if (document.getElementById("2").innerHTML === '<span class="x">X</span>' && document.getElementById("5").innerHTML === '<span class="x">X</span>' && document.getElementById("8").innerHTML === '<span class="x">X</span>') {
                 document.getElementById('result-message').innerHTML = "You won!";
+                winnerState = true;
             } else if (document.getElementById("2").innerHTML === '<span class="o">O</span>' && document.getElementById("5").innerHTML === '<span class="o">O</span>' && document.getElementById("8").innerHTML === '<span class="o">O</span>') {
                 document.getElementById('result-message').innerHTML = "You won!";
+                winnerState = true;
             } else if (document.getElementById("3").innerHTML === '<span class="x">X</span>' && document.getElementById("6").innerHTML === '<span class="x">X</span>' && document.getElementById("9").innerHTML === '<span class="x">X</span>') {
                 document.getElementById('result-message').innerHTML = "You won!";
+                winnerState = true;
             } else if (document.getElementById("3").innerHTML === '<span class="o">O</span>' && document.getElementById("6").innerHTML === '<span class="o">O</span>' && document.getElementById("9").innerHTML === '<span class="o">O</span>') {
                 document.getElementById('result-message').innerHTML = "You won!";
+                winnerState = true;
             } else if (document.getElementById("1").innerHTML === '<span class="x">X</span>' && document.getElementById("5").innerHTML === '<span class="x">X</span>' && document.getElementById("9").innerHTML === '<span class="x">X</span>') {
                 document.getElementById('result-message').innerHTML = "You won!";
+                winnerState = true;
             } else if (document.getElementById("1").innerHTML === '<span class="o">O</span>' && document.getElementById("5").innerHTML === '<span class="o">O</span>' && document.getElementById("9").innerHTML === '<span class="o">O</span>') {
                 document.getElementById('result-message').innerHTML = "You won!";
+                winnerState = true;
             } else if (document.getElementById("3").innerHTML === '<span class="x">X</span>' && document.getElementById("5").innerHTML === '<span class="x">X</span>' && document.getElementById("7").innerHTML === '<span class="x">X</span>') {
                 document.getElementById('result-message').innerHTML = "You won!";
+                winnerState = true;
             } else if (document.getElementById("3").innerHTML === '<span class="o">O</span>' && document.getElementById("5").innerHTML === '<span class="o">O</span>' && document.getElementById("7").innerHTML === '<span class="o">O</span>') {
                 document.getElementById('result-message').innerHTML = "You won!";
+                winnerState = true;
             }
         }
+
+        function showRestartButton() {
+
+            if (winnerState) {
+                document.getElementById("restart").insertAdjacentHTML("afterbegin", '<button>Restart</button>');
+            }
+
+            document.getElementById("restart").addEventListener("click", function () {
+
+                for (let i = 1; i <= 9; i++) {
+
+                    document.getElementById(i.toString()).innerHTML = "";
+                }
+
+                document.getElementById("restart").innerHTML = "";
+
+                winnerState = false;
+
+
+
+            });
+        }
+
+
+
+
+
+
 
         function addLetter() {
 
@@ -50,16 +95,15 @@ document.addEventListener('DOMContentLoaded', function () {
                     document.getElementById(i.toString()).insertAdjacentHTML("afterbegin", '<span class="x">X</span>');
                     playerTurnState = false;
                     checkWinner();
+                    showRestartButton();
                 }
 
             } else if ((!document.getElementById(i.toString()).innerHTML)) {
                 document.getElementById(i.toString()).insertAdjacentHTML("afterbegin", '<span class="o">O</span>');
                 playerTurnState = true;
                 checkWinner();
-
-
+                showRestartButton();
             }
-
 
         }
     }
